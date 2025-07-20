a864344cdd35960cb6dd2cef45bfbd8606b6b560
Mounika123-N <mounimunni2001@gmail.com>
2025-07-20 16:34:49 +0530

    Add files via upload

---
+++ JobPortalFinal/src/main/java/com/jobportal/Entities/Application.java
@@ -0,0 +1,30 @@
+package com.jobportal.Entities;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Application {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long applicationId;
+	@ManyToOne
+	private Job job;
+	@ManyToOne
+	private Users users;
+	private LocalDateTime appliedAt;
+	private String resume;
+
+}
+
